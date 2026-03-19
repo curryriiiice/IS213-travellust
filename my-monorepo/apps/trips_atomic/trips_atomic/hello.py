4712b2d267c28032d111e40dbcff917261a53e71
influasher <laiu.asher@gmail.com>
2026-03-19 13:11:35 +0800

    initial flask application

--- my-monorepo/apps/trips_atomic/trips_atomic/hello.py
+++ my-monorepo/apps/trips_atomic/trips_atomic/hello.py
@@ -1,6 +1,14 @@
 """Sample Hello World application."""
+from flask import Flask
+
+app = Flask(__name__)
 
 
+@app.route("/")
 def hello():
     """Return a friendly greeting."""
     return "Hello trips_atomic"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
